0817008341437f105561b1079b2b8b6906ea6c4c
Spinkll <roma.skorohod.2005@gmail.com>
2024-08-11 14:19:21 +0300

    fix2

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -34,18 +34,18 @@ export const createContact = async (payload) => {
   return contact;
 };
 
-export const updateContact = async (contactId, payload, options) => {
-  const rawResult = await ContactsCollection.findByIdAndUpdate(
+export const updateContact = async (contactId, payload) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
     { _id: contactId },
     payload,
-    { new: true, includeResultMetadata: true, ...options },
+    { new: true, includeResultMetadata: true },
   );
 
   if (!rawResult || !rawResult.value) return null;
 
   return {
     contact: rawResult.value,
-    isNew: Bollean(rawResult?.lastErrorObject?.upserted),
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
   };
 };
 
